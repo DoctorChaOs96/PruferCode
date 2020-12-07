7af2a949f6cfa0035852e6caf42392513fe8d24c
Oleksandr But <doctorhaos96@gmail.com>
2020-12-07 22:07:27 +0200

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        private class Node
+        {
+            private Node _parent;
+            private List<Node> _children;
+
+            public Node(int value)
+            {
+                _parent = null;
+                _children = new List<Node>();
+                Value = value;
+            }
+
+            public int Value { get; }
+
+            public bool IsTaken()
+            {
+                return _parent != null;
+            }
+
+            public bool IsRelated(int relation)
+            {
+                return _parent?.Value == relation || _children.Any(ch => ch.Value == relation);
+            }
+
+            public void AssignParent(Node relation)
+            {
+                _parent = relation;
+            }
+
+            public void AssignChild(Node relation)
+            {
+                _children.Add(relation);
+            }
+
+            public IEnumerable<int> GetAllRelations()
+            {
+                var outList = new List<Node>(1000);
+                
+                if(_parent != null)
+                    outList.Add(_parent);
+                
+                _children.ForEach(ch => outList.Add(ch));
+
+                return outList.OrderBy(n => n.Value).Select(n => n.Value);
+            }
+
+            public override string ToString()
+            {
+                return this.Value.ToString();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            //var baseApexesValues = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var baseApexesValues = "2 1 6 2 6".Split(' ').Select(int.Parse).ToArray();
+            
+            var nodes = Enumerable.Range(1, baseApexesValues.Length + 1).Select(x => new Node(x)).ToArray();
+            
+            var baseNodes = baseApexesValues.Select(bnv => nodes.First(n => n.Value == bnv)).ToArray();
+            var boundaryNodes = nodes.Where(n => !baseNodes.Any(bn => bn.Value == n.Value)).ToArray();
+
+            for (int i = 0; i < baseNodes.Length; i++)
+            {
+                var baseNode = baseNodes[i];
+
+                var possibleNodes = new List<Node>(boundaryNodes.Where(n => !n.IsTaken()));
+                
+                for (int j = 0; j < i; j++)
+                {
+                    var freeBaseNode = baseNodes[j];
+
+                    if (freeBaseNode.IsTaken())
+                    {
+                        continue;
+                    }
+                    
+                    bool willBeUsed = false;
+                    
+                    for (int k = i; k < baseNodes.Length; k++)
+                    {
+                        if (baseNodes[k].Value == freeBaseNode.Value)
+                        {
+                            willBeUsed = true;
+                            break;
+                        }
+                    }
+                    
+                    if(!willBeUsed)
+                        possibleNodes.Add(freeBaseNode);
+                }
+
+
+                Node lowestNode = new Node(75002);
+                
+                foreach (var node in possibleNodes)
+                {
+                    if (lowestNode.Value > node.Value)
+                    {
+                        lowestNode = node;
+                    }
+                }
+                
+                lowestNode.AssignParent(baseNode);
+                baseNode.AssignChild(lowestNode);
+            }
+
+            foreach (var node in nodes)
+            {
+                Console.WriteLine($"{node.Value}: {string.Join(" ", nodes.Where(n => n.IsRelated(node.Value)))}");
+                //Console.WriteLine($"{node.Value}: {string.Join(" ", node.GetAllRelations())}");
+            }
+
+            //Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ConsoleApplication1
+{
+    [SuppressMessage("ReSharper", "TooWideLocalVariableScope")]
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            #region Buffer
+
+            int counter1, counter2;
+
+            #endregion
+
+            #region Fill apexes
+
+            
+            //var input = Console.ReadLine();
+            var input = "2 1 6 2 6";
+            
+            
+            // ReSharper disable once PossibleNullReferenceException
+            var apexSet = input.Split(' ').Select(x => int.Parse(x)).ToArray();
+            var apexesCount = apexSet.Count();
+            var apexes = new int[apexesCount, 2];
+            
+            for (counter1 = 0; counter1 < apexesCount; counter1++)
+            {
+                apexes[counter1, 0] = apexSet[counter1];
+            }
+            
+            #endregion
+            
+            
+            // [x, 0] - value 
+            // [x, 1] - isFreeNode
+            // [x, 2] - relation
+
+            var nodesCount = 0; // max value in apexes array
+            
+            for (counter1 = 0; counter1 < apexes.Length; counter1++)
+            {
+                if (apexes[counter1, 0] > nodesCount)
+                    nodesCount = apexes[counter1, 0];
+            }
+
+            //var nodes = new int[nodesCount, 3];
+
+            bool isFound;
+            var freeNodes = new List<int[]>(3000);
+            
+            for (counter1 = 1; counter1 <= nodesCount; counter1++)
+            {
+                isFound = false;
+                
+                for (counter2 = 0; counter2 < apexes.Length; counter2++)
+                {
+                    if (apexes[counter2, 0] == counter1)
+                    {
+                        isFound = true;
+                        break;
+                    }
+                }
+                
+                if(!isFound)
+                    freeNodes.Add(new [] { counter1, 0 });
+            }
+            
+            //////////////////////////////////////////////////////////////////////////////////////////////////////
+            
+            int minimalNode;
+
+            bool isFreeNodeUsed = false;
+            int elementIndexThatWasUsed = -1;
+            
+            for (counter1 = 0; counter1 < apexesCount; counter1++)
+            {
+                minimalNode = nodesCount + 1;
+                
+                // Finds minimal free node
+                for (counter2 = 0; counter2 < freeNodes.Count; counter2++)
+                {
+                    if (freeNodes[counter2][1] == 0 && minimalNode > freeNodes[counter2][0])
+                    {
+                        minimalNode = freeNodes[counter2][0];
+                        isFreeNodeUsed = true;
+                        elementIndexThatWasUsed = counter2;
+                    }
+                }
+                
+                // Compares with lowest apex
+                bool isUsed;
+                
+                for (counter2 = 0; counter2 < counter1; counter2++)
+                {
+                    isUsed = false;
+                    
+                    if (apexes[counter2, 1] == 0 && apexes[counter2, 0] < minimalNode)
+                    {
+                        for (int counter3 = counter2 + 1; counter3 < apexesCount; counter3++)
+                        {
+                            if (apexes[counter3, 0] == apexes[counter2, 0])
+                            {
+                                isUsed = true;
+                                break;
+                            }
+                        }
+                        
+                        if(!isUsed)
+                        {
+                            minimalNode = apexes[counter2, 0];
+                            isFreeNodeUsed = false;
+                            elementIndexThatWasUsed = counter2;
+                        }
+                    }
+                }
+                
+                if (isFreeNodeUsed)
+                {
+                    apexes[elementIndexThatWasUsed, 1] = minimalNode;
+                }
+                else
+                {
+                    freeNodes[elementIndexThatWasUsed][1] = minimalNode;
+                }
+            }
+
+            
+            
+            for (counter1 = 0; counter1 < nodesCount; counter1++)
+            {
+                var relationList = new List<int>(100);
+                
+                for (counter2 = 0; counter2 < freeNodes.Count; counter2++)
+                {
+                    if (freeNodes[counter2][0] == counter1)
+                    {
+                        relationList.Add(freeNodes[counter2][1]);
+                    } else if (freeNodes[counter2][1] == counter1)
+                    {
+                        relationList.Add(freeNodes[counter2][0]);
+                    }
+                }
+
+                for (counter2 = 0; counter2 < apexesCount; counter2++)
+                {
+                    if (apexes[counter2, 0] == counter1)
+                    {
+                        relationList.Add(apexes[counter2, 1]);
+                    } else if (apexes[counter2, 1] == counter1)
+                    {
+                        relationList.Add(apexes[counter2, 0]);
+                    }
+                }
+                
+                var strBuilder = new StringBuilder();
+
+                strBuilder.Append(counter1);
+                strBuilder.Append(": ");
+                
+                for (counter2 = 0; counter2 < relationList.Count; counter2++)
+                {
+                    strBuilder.Append(relationList[counter2]);
+                    strBuilder.Append(" ");
+                }
+                
+                Console.WriteLine(strBuilder.ToString().Trim());
+            }
+        }
+    }
+}
